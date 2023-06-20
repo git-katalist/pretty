6571661ede6cac3cfdd78c21bc19646cb04c6b7e
tsots <mosalatsotetsi@gmail.com>
2023-06-20 12:36:19 +0200

    publish site second trial

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://git-katalist.github.io",
-  base: "/pretty",
+  //   site: "https://git-katalist.github.io",
+  //   base: "/pretty",
 });
